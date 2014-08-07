e704562b4c45a14c8e0c8fed786c4c53611fe392
vleango <vleango@gmail.com>
2014-08-07 13:30:41 +0900

    add ios

---
+++ src/ios/TextArea.h
@@ -0,0 +1,14 @@
+//
+//  TextArea.h
+//
+//  Created by Tha Leang on 8/7/14.
+//
+//
+
+#import <Cordova/CDV.h>
+
+@interface TextArea : CDVPlugin
+
+- (void)openTextView:(CDVInvokedUrlCommand*)command;
+
+@end
---
+++ src/ios/TextArea.m
@@ -0,0 +1,16 @@
+//
+//  TextArea.m
+//
+//  Created by Tha Leang on 8/7/14.
+//
+//
+
+#import "TextArea.h"
+
+@implementation TextArea
+
+- (void)openTextView:(CDVInvokedUrlCommand*)command {
+
+}
+
+@end
